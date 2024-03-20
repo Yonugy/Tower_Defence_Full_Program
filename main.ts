af0cc8de0de158863e81a5826ce2ac8edadc93f7
Yonugy <65732630+Yonugy@users.noreply.github.com>
2024-03-20 18:33:01 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,172 @@
+enum SpriteKindLegacy {
+    Player,
+    Projectile,
+    Food,
+    Enemy
+}
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    子弹 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . c a a a c . . . . . . . 
+        . . . c c f a b b c . . . . . . 
+        . . . b f f b f a a . . . . . . 
+        . . . b b a b f f a . . . . . . 
+        . . . c b f b b a c . . . . . . 
+        . . . . b a f c c . . . . . . . 
+        . . . . . b b c . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Tower, 0, -100)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    music.pewPew.play()
+    info.changeScoreBy(1)
+    otherSprite.destroy(effects.disintegrate, 500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    music.powerDown.play()
+    info.changeLifeBy(-1)
+    // 不要忘记销毁，以及销毁的对象应该是othersprite。
+    otherSprite.destroy()
+    Tower.startEffect(effects.fire, 1000)
+    Tower.say("Hit! Danger!", 2000)
+})
+let MonsterAnimation: animation.Animation = null
+let Monster: Sprite = null
+let 子弹: Sprite = null
+let Tower: Sprite = null
+info.setLife(3)
+game.showLongText("Game Rules: Control the tower using buttons to avoid monster attacks. The tower can only withstand three attacks!", DialogLayout.Center)
+effects.starField.startScreenEffect()
+effects.blizzard.startScreenEffect()
+Tower = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 6 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 6 . . . . . . . 
+    . . . . . . 8 8 9 8 . . . . . . 
+    . . . . . . 8 6 9 8 . . . . . . 
+    . . . . . c c c 8 8 8 . . . . . 
+    . . . . 8 8 6 6 6 9 8 8 . . . . 
+    . . 8 f f f c c e e f f 8 8 . . 
+    . 8 8 8 8 8 8 6 6 6 6 9 6 8 8 . 
+    8 8 8 8 8 8 8 8 6 6 6 9 6 6 8 8 
+    8 8 8 8 8 8 8 8 6 6 6 6 9 6 8 8 
+    `, SpriteKind.Player)
+Tower.bottom = 120
+controller.moveSprite(Tower, 100, 0)
+Tower.setFlag(SpriteFlag.StayInScreen, true)
+game.onUpdateInterval(1000, function () {
+    Monster = sprites.createProjectileFromSide(img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c b b b b b b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b c b b b c b b b b f . . . . 
+        f b 1 f f f 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `, 0, 50)
+    Monster.x = randint(0, 160)
+    Monster.setKind(SpriteKind.Enemy)
+    MonsterAnimation = animation.createAnimation(ActionKind.Walking, 1000)
+    MonsterAnimation.addAnimationFrame(img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c b b b b b b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b c b b b c b b b b f . . . . 
+        f b 1 f f f 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `)
+    MonsterAnimation.addAnimationFrame(img`
+        . . f f f . . . . . . . . . . . 
+        f f f c c . . . . . . . . f f f 
+        f f c c . . c c . . . f c b b c 
+        f f c 3 c c 3 c c f f b b b c . 
+        f f b 3 b c 3 b c f b b c c c . 
+        . c b b b b b b c f b c b c c . 
+        . c b b b b b b c b b c b b c . 
+        c b 1 b b b 1 b b b c c c b c . 
+        c b b b b b b b b c c c c c . . 
+        f b c b b b c b b b b f c . . . 
+        f b 1 f f f 1 b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    MonsterAnimation.addAnimationFrame(img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c 1 b b b 1 b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b 1 f f 1 c b b b b f . . . . 
+        f f 1 f f 1 f b b b b f c . . . 
+        f f 2 2 2 2 f b b b b f c c . . 
+        . f 2 2 2 2 b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `)
+    animation.attachAnimation(Monster, MonsterAnimation)
+    animation.setAction(Monster, ActionKind.Walking)
+})
+forever(function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+})
+forever(function () {
+    if (info.life() == 2) {
+        scene.setBackgroundColor(4)
+        controller.moveSprite(Tower, 50, 0)
+        Monster.setVelocity(0, 70)
+    } else if (info.life() == 1) {
+        scene.setBackgroundColor(2)
+        controller.moveSprite(Tower, 20, 0)
+        Monster.setVelocity(0, 90)
+    }
+})
+forever(function () {
+    if (info.score() == 30) {
+        game.showLongText("You have successfully defended the defence tower and Meow Meow Planet!", DialogLayout.Bottom)
+        game.over(true, effects.confetti)
+    }
+})
